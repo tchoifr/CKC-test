39f49adfe3049b949fdd2d860ed9b2d805dcf2ef
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2024-04-09 02:45:11 +0200

    accueil avec style général

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Accueil from "@/pages/Accueil";
+const page = () => {
+  return (
+    <div>
+      <Accueil />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/pages/Accueil.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header'
+import NavBar from '@/components/NavBar'
+import React from 'react'
+
+const accueil = () => {
+  return (
+    <div>
+     <NavBar />
+     <Header />   
+    </div>
+  )
+}
+
+export default accueil
