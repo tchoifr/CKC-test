70a761d9b5a9e79f7fea6d70988c072b36a3de13
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2024-04-09 16:51:02 +0200

    detail bas de page

--- src/components/BasDePageImg.tsx
+++ src/components/BasDePageImg.tsx
@@ -5,10 +5,10 @@ const BasDePageImg = () => {
     <div className=" p-40 bg-white padding-mobile-bas-de-page">
       <div className="flex flex-col justify-center items-center img-Bateau-blanc-et-bleu padding-mobile-bas-de-page">
         <p className="p-3 custom-color">LOREM IPSUM</p>
-        <h3 className="p-3 font-bold text-5xl text-mobile">
-          Lorem ipsum dolor sit amet, consectetur adipiscing.
+        <h3 className="p-3 font-bold text-5xl text-mobile text-center">
+          Lorem ipsum dolor sit amet, <br /> consectetur adipiscing.
         </h3>
-        <p>
+        <p className="p-5">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore
         </p>
