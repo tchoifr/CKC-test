42408a2f7f24e593bc8c0e5e4de5f431d72ba07f
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2024-04-09 12:44:58 +0200

    integration description header

---
+++ src/components/DescriptionHeaderText.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const DescriptionHeaderText = () => {
+  return (
+    <div className="bg-custom-color flex justify-center text-center flex-col items-center">
+      <div>
+        <h2 className="text-5xl p-4">
+          Lorem ipsum dolor sit amet, <br /> consectetur adipiscing.
+        </h2>
+        <p className="p-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation <br /> ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionHeaderText;
